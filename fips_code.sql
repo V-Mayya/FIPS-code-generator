368b3d213cf849414403a55b837a4505566e66ec
V-Mayya <87191173+V-Mayya@users.noreply.github.com>
2022-10-09 00:18:10 +0100

    Update fips_code.sql

--- fips_code.sql
+++ fips_code.sql
@@ -1,3 +1,5 @@
+-- import data from three tables: 'other', 'countries' and 'statecode', each of which contain the stated columns below: 
+
 -- County name from city name, state name (table has has three columns: city name, state name which is state full and county name) 
 SELECT * from other WHERE city = " " and `state full` = " ";
 
@@ -8,9 +10,9 @@ SELECT FIPS, Name from countries WHERE state = " " and Name = " ";
 SELECT * from statecode;  
 
 -- Creating variable names for city name and state name as user's input from excel sheet
-SET @city_name = "Terrell";
-SET @state_name = "texas";
+SET @city_name = " ";
+SET @state_name = " ";
 
--- using inputs of state and city name to get final output of county name (which is 'Name') and fips code 
+-- using inputs of state and city name to get final output of county name (which is 'Name') and FIPS code 
 SELECT FIPS, Name from countries WHERE state IN (SELECT abbreviation FROM statecode WHERE name = @state_name) and Name IN (SELECT county from other WHERE city = @city_name and `state full` = @state_name);
 
